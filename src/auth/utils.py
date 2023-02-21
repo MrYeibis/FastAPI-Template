813829dd5d27b4609f1dd0acf0490ca2451e3fa5
MrYeibis <yeibisrojas@hotmail.com>
2023-02-21 09:21:11 -0500

    Requirements Updated

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -46,7 +46,6 @@ class UsersHandler():
     def get_user(db: Session, user_id: int):
         return db.query(models.User).filter(models.User.id == user_id).first()
 
-
     def get_user_by_email(db: Session, email: str):
         return db.query(models.User).filter(models.User.email == email).first()
 
